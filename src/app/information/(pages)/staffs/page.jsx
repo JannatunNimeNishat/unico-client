3bf57c7963a84597790c6b1e8da54eb3ac5dbf5e
JannatunNimeNishat <nishatnime100@gmail.com>
2023-09-14 03:28:17 +0600

    staffs page added

--- src/app/information/(pages)/staffs/page.jsx
+++ src/app/information/(pages)/staffs/page.jsx
@@ -1,10 +1,148 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const StaffsPage = () => {
     return (
         <div>
-            StaffsPage
+        {/* banner */}
+        <div className="bg-[url('/staffs/Staffs.jpg')] h-[40vh] bg-cover bg-center">
+            <div className='backdrop-brightness-50 w-full h-full flex flex-col  px-8 justify-center'>
+                <h3 className='text-5xl font-bold text-white '>Faculty & Staffs</h3>
+                <div className='h-1 w-[200px]  bg-[#ae152d] mt-[12px] mb-[20px] ' />
+            </div>
+
         </div>
+
+        <div className='mt-20'>
+            <h3 className='text-3xl font-bold text-center w-2/3 mx-auto leading-relaxed'>Resources, offices and services that support UNICO faculty and staff</h3>
+
+            {/* academic resources and campus life */}
+            <div className='flex gap-8 my-container mt-12'>
+                {/* 1st */}
+                <div className='flex-1'>
+                    <Image className='h-[300px] w-[580px]' height={200} width={500} src={'/staffs/Staffs1.jpg'} alt='' />
+                    <div className='mt-8 '>
+                        <h3 className='text-3xl font-bold'>Staff Benefits & Resources</h3>
+                        <p className='mt-5  text-gray-500'>All of the information about resources available, and skills development <br /> options to help you while at UNICO.</p>
+                        <ul className='list-disc mt-3 px-8 flex gap-24 text-gray-500'>
+                            <div>
+                                <li className='py-2 hover:text-[#ae152d]'>
+                                    <Link href={`#`}>Academic Calender</Link>
+                                </li>
+                                <li className='py-2 hover:text-[#ae152d]'>
+                                    <Link href={`#`}>Exam Schedule</Link>
+                                </li>
+                                <li className='py-2 hover:text-[#ae152d]'>
+                                    <Link href={`#`}>Exam Schedule</Link>
+                                </li>
+                                <li className='py-2 hover:text-[#ae152d]'>
+                                    <Link href={`#`}>Academic Advising</Link>
+                                </li>
+                                <li className='py-2 hover:text-[#ae152d]'>
+                                    <Link href={`#`}>Course Offering</Link>
+                                </li>
+                            </div>
+                            <div>
+                                <li className='py-2 hover:text-[#ae152d]'>
+                                    <Link href={`#`}>Blackboard</Link>
+                                </li>
+                                <li className='py-2 hover:text-[#ae152d]'>
+                                    <Link href={`#`}>Library</Link>
+                                </li>
+                                <li className='py-2 hover:text-[#ae152d]'>
+                                    <Link href={`#`}>Career Services</Link>
+                                </li>
+                            </div>
+                        </ul>
+                    </div>
+
+                </div>
+                {/* 2nd */}
+                <div className='flex-1'>
+                    <Image className='h-[300px] w-[580px]' height={200} width={500} src={'/staffs/Staffs3.jpg'} alt='' />
+                    <div className='mt-8'>
+                        <h3 className='text-3xl font-bold'>Faculty Resources</h3>
+                        <p className='mt-5 text-gray-500'>Everything you need to know about term dates, transport, <br /> accommodation, as well as social activities.</p>
+                        <ul className='list-disc mt-3 px-8 text-gray-500'>
+
+                            <li className='py-2 hover:text-[#ae152d]'>
+                                <Link href={`#`}>Housing & Dining</Link>
+                            </li>
+                            <li className='py-2 hover:text-[#ae152d]'>
+                                <Link href={`#`}>Community</Link>
+                            </li>
+                            <li className='py-2 hover:text-[#ae152d]'>
+                                <Link href={`#`}>Student Group</Link>
+                            </li>
+                            <li className='py-2 hover:text-[#ae152d]'>
+                                <Link href={`#`}>Getting Around</Link>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+
+            {/* Health & Well-Being and Financials */}
+            <div className='flex gap-8 my-container mt-12'>
+                {/* 1st */}
+                <div className='flex-1'>
+                    <Image className='h-[300px] w-[580px]' height={200} width={500} src={'/staffs/Staffs4.jpg'} alt='' />
+                    <div className='mt-8 '>
+                        <h3 className='text-3xl font-bold'>Academics</h3>
+                        <p className='mt-5  text-gray-500'>These pages provide information to empower you to manage your health <br /> and wellbeing.</p>
+                        <ul className='list-disc mt-3 px-8  text-gray-500'>
+
+                            <li className='py-2 hover:text-[#ae152d]'>
+                                <Link href={`#`}>Emergency</Link>
+                            </li>
+                            <li className='py-2 hover:text-[#ae152d]'>
+                                <Link href={`#`}>Psychological Services</Link>
+                            </li>
+                            <li className='py-2 hover:text-[#ae152d]'>
+                                <Link href={`#`}>University Health Services</Link>
+                            </li>
+                            <li className='py-2 hover:text-[#ae152d]'>
+                                <Link href={`#`}>Health Insurance</Link>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                </div>
+                {/* 2nd */}
+                <div className='flex-1'>
+                    <Image className='h-[300px] w-[580px]' height={200} width={500} src={'/staffs/Staffs5.jpg'} alt='' />
+                    <div className='mt-8'>
+                        <h3 className='text-3xl font-bold'>Research</h3>
+                        <p className='mt-5 text-gray-500'>Information about student fees, funding opportunities, financial <br /> assistance, and student loans.</p>
+                        <ul className='list-disc mt-3 px-8 text-gray-500'>
+
+                            <li className='py-2 hover:text-[#ae152d]'>
+                                <Link href={`#`}>Tuition and Fees</Link>
+                            </li>
+                            <li className='py-2 hover:text-[#ae152d]'>
+                                <Link href={`#`}>Financial Aid Service</Link>
+                            </li>
+                            <li className='py-2 hover:text-[#ae152d]'>
+                                <Link href={`#`}>Fellowships, Opportunities, Grants, and Scholarship</Link>
+                            </li>
+                            <li className='py-2 hover:text-[#ae152d]'>
+                                <Link href={`#`}>Personal Finance</Link>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
     );
 };
 
