fa1cfc2af18d985d885d36c95b99d9a07222b29a
JannatunNimeNishat <nishatnime100@gmail.com>
2023-10-28 20:49:56 +0600

    banner section added on Inspiring Student life page

--- src/app/inspiringStudentLife/page.jsx
+++ src/app/inspiringStudentLife/page.jsx
@@ -4,6 +4,10 @@ const InspiringStudentLife = () => {
     return (
         <div>
             <h3>Inspiring Student Life</h3>
+            {/* banner */}
+            <section>
+                <h3>Banner section</h3>
+            </section>
         </div>
     );
 };
